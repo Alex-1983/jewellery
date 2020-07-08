03e6a8ffcd5df39265c2f391b5a53b1a15b94d4a
Alex-1983 <39596822+Alex-1983@users.noreply.github.com>
2020-07-08 20:40:22 +0500

    добавляет slider

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 (function () {
+
+  // menu
+
   var body = document.querySelector('body');
   var header = document.querySelector('.header');
   var burger = document.querySelector('.burger');
@@ -11,5 +14,56 @@
       body.classList.toggle('no-scroll');
     });
   }
+
+  // slider
+
+  var slider = document.querySelector('.slider');
+  var sliderBtnPrevious = slider.querySelector('.arrow--prev');
+  var sliderBtnNext = slider.querySelector('.arrow--next');
+  var sliderPagination = slider.querySelector('.swiper-pagination');
+
+  window.mySwipper = new window.Swiper('.swiper-container', {
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    spaceBetween: 30,
+    loop: true,
+
+    pagination: {
+      el: sliderPagination,
+      clickable: true,
+
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
+    },
+
+    breakpoints: {
+      1023: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+
+      320: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 30,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          type: 'custom',
+          renderCustom: function (swiper, prev, next) {
+            return '<div class="swiper-pagination-sm"><span>' + prev + '</span> <span>of</span> <span>' + next + '</span></div>';
+          }
+        }
+      },
+    },
+
+    navigation: {
+      nextEl: sliderBtnNext,
+      prevEl: sliderBtnPrevious
+    },
+
+
+  });
 
 })();
