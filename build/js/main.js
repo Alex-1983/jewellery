f2c9ba0658baae202a5d9780e28df9652ed5d612
Alex-1983 <39596822+Alex-1983@users.noreply.github.com>
2020-07-13 22:08:53 +0500

    добавляет каталог

--- build/js/main.js
+++ build/js/main.js
@@ -1,94 +1,180 @@
 'use strict';
 
-(function () {
+(function() {
+
+  var ESC_KEYCODE = 27;
 
   document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
 
-  // menu
-
-  var body = document.querySelector('body');
-  var header = document.querySelector('.header');
-  var burger = document.querySelector('.burger');
-
-  if (burger) {
-    burger.addEventListener('click', function () {
-      header.classList.toggle('header--open');
-      body.classList.toggle('no-scroll');
-    });
-  }
-
-  // slider
-
-  var slider = document.querySelector('.slider');
-  var sliderBtnPrevious = slider.querySelector('.arrow--prev');
-  var sliderBtnNext = slider.querySelector('.arrow--next');
-  var sliderPagination = slider.querySelector('.swiper-pagination');
-
-  window.mySwipper = new window.Swiper('.swiper-container', {
-    slidesPerView: 4,
-    slidesPerGroup: 4,
-    spaceBetween: 30,
-    loop: true,
-
-    pagination: {
-      el: sliderPagination,
-      clickable: true,
-
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + '</span>';
-      },
-    },
-
-    breakpoints: {
-      1023: {
-        slidesPerView: 2,
-        slidesPerGroup: 2,
-      },
-
-      320: {
-        slidesPerView: 2,
-        slidesPerGroup: 2,
-        spaceBetween: 30,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-          type: 'fraction',
-          renderFraction: function (currentClass, totalClass) {
-            return '<span class="' + currentClass + '"></span>' + ' of ' + '<span class="' + totalClass + '"></span>';
-          }
-        }
-      },
-    },
-
-    navigation: {
-      nextEl: sliderBtnNext,
-      prevEl: sliderBtnPrevious
-    },
-
-
-  });
-
-  // faq
-
-  var faq = document.querySelector('.faq');
-  var faqToggles = faq.querySelectorAll('.faq__toggle');
-  var faqContent = faq.querySelectorAll('.faq__item p');
-
-  var onToggleClicked = function (evt) {
-    var faqContainer = evt.target.closest('.faq__title').parentElement;
-    if (faqContainer.classList.contains('faq__item--open')) {
-      faqContainer.classList.remove('faq__item--open');
-    } else {
-      for (var i = 0; i < faqContent.length; i++) {
-        faqContent[i].classList.remove('faq__item--open');
+  var createEscHandler = function(fn) {
+    return function(evt) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        fn();
       }
-      faqContainer.classList.add('faq__item--open');
-    }
+    };
   };
 
-  for (var i = 0; i < faqToggles.length; i++) {
-    faqToggles[i].addEventListener('click', onToggleClicked);
-  }
+  window.utils = {
+    createEscHandler: createEscHandler
+  };
+
+  (function() {
+
+    // menu
+
+    var body = document.querySelector('body');
+    var header = document.querySelector('.header');
+    var burger = document.querySelector('.burger');
+
+    if (burger) {
+      burger.addEventListener('click', function() {
+        header.classList.toggle('header--open');
+        body.classList.toggle('no-scroll');
+      });
+    }
+
+  })();
+
+  (function() {
+
+    // slider
+
+    window.mainSwipper = new window.Swiper('.swiper-container', {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 30,
+      loop: true,
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+
+        renderBullet: function(index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+      },
+
+      breakpoints: {
+        1023: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+        },
+
+        320: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 30,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            type: 'fraction',
+            renderFraction: function(currentClass, totalClass) {
+              return '<span class="' + currentClass + '"></span>' + ' of ' + '<span class="' + totalClass + '"></span>';
+            }
+          }
+        },
+      },
+
+      navigation: {
+        nextEl: '.arrow--next',
+        prevEl: '.arrow--prev'
+      },
+
+    });
+
+  })();
+
+  (function() {
+
+    // faq
+
+    var faqToggles = document.querySelectorAll('.faq__toggle');
+    var faqContent = document.querySelectorAll('.faq__item p');
+
+    if (faqToggles) {
+      var onTFaqTogglesClick = function(evt) {
+        var faqContainer = evt.target.closest('.faq__title').parentElement;
+        if (faqContainer.classList.contains('faq__item--open')) {
+          faqContainer.classList.remove('faq__item--open');
+        } else {
+          for (var i = 0; i < faqContent.length; i++) {
+            faqContent[i].classList.remove('faq__item--open');
+          }
+          faqContainer.classList.add('faq__item--open');
+        }
+      };
+
+      for (var i = 0; i < faqToggles.length; i++) {
+        faqToggles[i].addEventListener('click', onTFaqTogglesClick);
+      }
+    }
+
+  })();
+
+  (function() {
+
+    // filter
+
+    var filterToggles = document.querySelectorAll('.filter__toggle');
+    var filterContent = document.querySelectorAll('.filter__content');
+
+    if (filterToggles) {
+      var onFilterTogglesClick = function(evt) {
+        var filterContainer = evt.target.closest('.filter__title').parentElement;
+        if (filterContainer.classList.contains('filter--open')) {
+          filterContainer.classList.remove('filter--open');
+        } else {
+          for (var i = 0; i < filterContent.length; i++) {
+            filterContent[i].classList.remove('filter--open');
+          }
+          filterContainer.classList.add('filter--open');
+        }
+      };
+
+      for (var i = 0; i < filterToggles.length; i++) {
+        filterToggles[i].addEventListener('click', onFilterTogglesClick);
+      }
+    }
+
+  })();
+
+  (function() {
+
+    // Open filter
+
+    var filter = document.querySelector('.filter');
+    var openFilterBtn = document.querySelector('.catalog__btn');
+    var closeFilterBtn = document.querySelector('.filter__close');
+
+     if (openFilterBtn && closeFilterBtn) {
+
+      var openFilter = function() {
+        filter.classList.add('filter-show');
+        openFilterBtn.classList.add('catalog__btn-close');
+      }
+
+      var closeFilter = function() {
+        filter.classList.remove('filter-show');
+        openFilterBtn.classList.remove('catalog__btn-close');
+      }
+
+      var onOpenFilterBtnClick = function() {
+        openFilter();
+        document.addEventListener('keydown', onEscDown);
+      }
+
+      var onCloseFilterBtnClick = function() {
+        closeFilter();
+        document.removeEventListener('keydown', onEscDown);
+      }
+
+      var onEscDown = window.utils.createEscHandler(onCloseFilterBtnClick);
+
+      openFilterBtn.addEventListener("click", onOpenFilterBtnClick);
+      closeFilterBtn.addEventListener("click", onCloseFilterBtnClick);
+    }
+
+  })();
 
   /*eslint-disable*/
   objectFitImages();
